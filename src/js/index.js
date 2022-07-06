a3e35ebcaa725fd6a671cef9633b68b59a4d70aa
Sussanara <sussanaracely@gmail.com>
2022-07-06 21:53:54 +0000

    css

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,19 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "bootstrap";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+export function Home () {
+    return (
+        <div className="traffic-Light">
+            <div className="Red">A</div>
+            <div className="Yellow">B</div>
+            <div className="Green">C</div>
+        </div>
+	);
+	}
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
