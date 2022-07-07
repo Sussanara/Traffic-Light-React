b10e7616c818da0911ab970a517ad387ced76125
Sussanara <sussanaracely@gmail.com>
2022-07-07 15:20:25 +0000

    CSS

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-import "../styles/index.css";
+import React, {useState}from "react";
 
 //create your first component
-
+function Home () {
+    const [ selectedColor, setSelectColor ] = useState("Red")
+    return (
+        <div className="traffic-Light">
+            <div className={"Red"+(selectedColor === "Red") ? "glow": ""} ></div>
+            <div className="Yellow"></div>
+            <div className="Green"></div>
+        </div>
+	);
+	}
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,13 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../styles/index.css'
+import Home from "./component/home.jsx";
 // include your styles into the webpack bundle
-import "../styles/index.css";
 
-export function Home () {
-    return (
-        <div className="traffic-Light">
-            <div className="Red">A</div>
-            <div className="Yellow">B</div>
-            <div className="Green">C</div>
-        </div>
-	);
-	}
+
+
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
